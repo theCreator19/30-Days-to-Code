5b28a7664391edfc98f8157f5d46ca6bea42b6b2
theCreator19 <77932869+theCreator19@users.noreply.github.com>
2021-12-12 21:08:41 +0530

    Create Day 1.java

---
+++ Day 1.java
@@ -0,0 +1,27 @@
+// Day 1
+
+// HackerRank Problem solved using Java.
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        Scanner sc=new Scanner(System.in);
+        int i=4;
+        double d=4.0;
+        String s= "HackerRank ";
+        int a= sc.nextInt();
+        
+        double b=sc.nextDouble();
+    
+        sc.nextLine();
+        String str= sc.nextLine();  
+        System.out.println(a+i);
+        System.out.println(b+d);
+        System.out.println(s+str);
+    }
+}
