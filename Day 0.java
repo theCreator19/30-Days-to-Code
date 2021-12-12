f3d1565fcc4b9c7dacccc7946f28e507e29c7294
theCreator19 <77932869+theCreator19@users.noreply.github.com>
2021-12-12 21:07:36 +0530

    Create Day 0.java

---
+++ Day 0.java
@@ -0,0 +1,16 @@
+// Day 0
+
+// HackerRank problem solved using Java.
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc= new Scanner(System.in);
+        String s=sc.nextLine();
+        System.out.println("Hello, World.\n"+s);
+    }
+}
